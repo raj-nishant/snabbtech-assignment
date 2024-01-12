6943c5c0d48087bedc3e736c970b27f9454fcf93
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-01-12 18:52:44 +0530

    commit

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,4 +1,5 @@
 import pics from "../assets/pics.png";
+import { Link } from "react-router-dom";
 
 import { useState } from "react";
 import Login from "./Login";
@@ -7,7 +8,7 @@ import Register from "./Register";
 const Auth = () => {
   const [showLogin, setShowLogin] = useState(false);
   const handleSignInClick = () => {
-    setShowLogin(true);
+    setShowLogin(!showLogin);
   };
 
   return (
@@ -19,20 +20,23 @@ const Auth = () => {
       <div className="flex max-w-[480px] w-full flex-col items-center mx-auto">
         <span className="items-stretch flex justify-between gap-5 px-5 pt-16">
           <span className="flex flex-col items-stretch">
-            <div className="text-stone-900 text-base leading-5 whitespace-nowrap hover:bg-white">
+            <Link
+              className="text-stone-900 text-base leading-5 whitespace-nowrap hover:bg-white"
+              onClick={handleSignInClick}
+            >
               Join PANDA
-            </div>
+            </Link>
           </span>
           <div className="text-gray-400 text-base leading-5 mt-1.5 self-start">
             or
           </div>
           <div>
-            <div
+            <Link
               className="text-stone-900 text-base leading-5 self-start hover:bg-white"
               onClick={handleSignInClick}
             >
               Sign in
-            </div>
+            </Link>
           </div>
         </span>
         {showLogin ? <Login /> : <Register />}
